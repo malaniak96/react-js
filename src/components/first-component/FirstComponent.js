c93dce4496270163d5e113aa33986cbd4a140d0c
OlgaM <139258143+malaniak96@users.noreply.github.com>
2023-10-15 23:38:46 +0300

    lesson1_1

---
+++ src/components/first-component/FirstComponent.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './FirstComponent.css';
+const FirstComponent = () => {
+
+    //js code will be here
+
+    return (
+        //розмітка
+        <div>
+            <p>Lorem ipsum </p>
+            <img src="https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png" alt= ""/>
+
+            <hr/>
+        </div>
+    );
+};
+
+export default FirstComponent;
