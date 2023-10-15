8267af647b6df2451ea591943f7f53bb9f2c1911
OlgaM <139258143+malaniak96@users.noreply.github.com>
2023-10-16 00:09:13 +0300

    lesson1_2

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import FirstComponent from "./components/first-component/FirstComponent";
 function App() {
   return (
     <div className="App">
-      <FirstComponent/>
-      <FirstComponent/>
+      <FirstComponent
+          txt={'Bart'}
+          image={'https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png'}/>
+      <FirstComponent
+          txt={'Homer'}
+          image={'https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png'}/>
     </div>
   );
 }
--- src/components/first-component/FirstComponent.js
+++ src/components/first-component/FirstComponent.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './FirstComponent.css';
-const FirstComponent = () => {
-
+const FirstComponent = ({txt: text, image}) => { //{txt:''}
     //js code will be here
 
     return (
         //розмітка
         <div>
-            <p>Lorem ipsum </p>
-            <img src="https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png" alt= ""/>
-
+            <h2>
+                {text}
+            </h2>
+            <img src={image} alt= ""/>
             <hr/>
         </div>
     );
