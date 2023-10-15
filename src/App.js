5f2c6dd60419382931571b2ab77b7228ee473889
OlgaM <139258143+malaniak96@users.noreply.github.com>
2023-10-16 00:56:35 +0300

    fetch lesson1_4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+
+  //const [users, setUsers] = useState([]);
+  //users = [] getter info
+  //setUsers([........]) setter info
+  //hook useState
+  // fetch('https://jsonplaceholder.typicode.com/users')
+  //     .then(value => value.json())
+  //     .then(value => {
+  //console.log('asd'); shows many times
+  //      setUsers(value);
+  //       //запускає рендер
+  //     });
+
+  //always use useEffect
+  //useState use for async or on click
+  // useEffect( ()=> {
+  //   fetch('https://jsonplaceholder.typicode.com/users')
+  //       .then(value => value.json())
+  //       .then(value => {
+  //         console.log('asd');
+  //        setUsers(value);
+  //       });
+  // }, []);
+  //
+  let [counter, setCounter] = useState(0);
+  let onClickIncrement = () => {
+    counter++;
+    setCounter(counter);
+    console.log(counter)
+  };
+  let onClickDecrement = () => {
+    counter--;
+    setCounter(counter);
+    console.log(counter);
+  };
+
   return (
     <div className="App">
+
+          <h1>counter: {counter}</h1>
+          <button onClick={onClickIncrement}>+</button>
+          <button onClick={onClickDecrement}>-</button>
 
     </div>
   );
 }
+//{users.map(value => <div> {value.name}</div>)}
 
 export default App;
