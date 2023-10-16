93076554b60414e008d0bbd45b1b4b22e86382b0
OlgaM <139258143+malaniak96@users.noreply.github.com>
2023-10-17 00:47:45 +0300

    lesson 1 hw2 using fetch

--- src/components/Simpsons.js
+++ src/components/Simpsons.js
@@ -1,51 +1,15 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Simpson} from "./Simpson";
 
 const Simpsons = () => {
-    const simpsons = [
-        {
-            "id": 1,
-            "name": "Rick Sanchez",
-            "status": "Alive",
-            "species": "Human",
-            "gender": "Male",
-            "image": "https://rickandmortyapi.com/api/character/avatar/1.jpeg"},
-        {
-            "id": 2,
-            "name": "Morty Smith",
-            "status": "Alive",
-            "species": "Human",
-            "gender": "Male",
-            "image": "https://rickandmortyapi.com/api/character/avatar/2.jpeg"},
-        {
-            "id": 3,
-            "name": "Summer Smith",
-            "status": "Alive",
-            "species": "Human",
-            "gender": "Female",
-            "image": "https://rickandmortyapi.com/api/character/avatar/3.jpeg"},
-        {
-            "id": 4,
-            "name": "Beth Smith",
-            "status": "Alive",
-            "species": "Human",
-            "gender": "Female",
-            "image": "https://rickandmortyapi.com/api/character/avatar/4.jpeg"},
-        {
-            "id": 5,
-            "name": "Jerry Smith",
-            "status": "Alive",
-            "species": "Human",
-            "gender": "Male",
-            "image": "https://rickandmortyapi.com/api/character/avatar/5.jpeg"},
-        {
-            "id": 6,
-            "name": "Abadango Cluster Princess",
-            "status": "Alive",
-            "species": "Alien",
-            "gender": "Female",
-            "image": "https://rickandmortyapi.com/api/character/avatar/6.jpeg"}
-    ];
+
+    const [simpsons, setSimpsons] = useState([]);
+
+    useEffect(() => {
+        fetch('https://rickandmortyapi.com/api/character?limit=6')
+            .then(value => value.json())
+            .then(character => setSimpsons(character))
+    }, []);
 
 
     return (
