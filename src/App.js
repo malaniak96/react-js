8c3777880eb2f6240bc14e7f6cc3e5f62ec5f956
OlgaM <139258143+malaniak96@users.noreply.github.com>
2023-10-18 21:57:16 +0300

    innit commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <div>
 
     </div>
   );
 }
 
-export default App;
+export {App};
