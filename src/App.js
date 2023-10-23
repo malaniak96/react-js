c7e74bc52af017f91f2031fe4130fdf003fae9c0
OlgaM <139258143+malaniak96@users.noreply.github.com>
2023-10-23 23:14:40 +0300

    lesson 3 part 1,2,3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import './App.css';
+import React, {useEffect, useState} from 'react';
+
 
 const App = () => {
-  return (
-    <div>
+    const [user, setUser] = useState({});
+    const [count, setCount] = useState(0);
 
-    </div>
-  );
-}
+    console.log(user);
+
+    // useEffect(() => {
+    //     const user = {
+    //         name: 'max',
+    //         age: 15
+    //     }
+    //     const nameKey = 'name'
+    //     const newUser = {...user, [nameKey]:'Kostia'}
+    // }, []);
+
+    return (
+      <div>
+          <div> count: {count}</div>
+          <button onClick={()=>setCount(prevState => prevState+1)}>inc</button>
+      </div>
+    );
+};
 
 export {App};
