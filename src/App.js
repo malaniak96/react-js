32cefbc624280397c57a9010b7be8de081e6bc14
OlgaM <139258143+malaniak96@users.noreply.github.com>
2023-11-22 23:33:49 +0200

    lesson10

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
+import {Users} from "./components/Users";
+import {useState} from "react";
+import {UserFc} from "./components/UserFC";
 
 const App = () => {
-  return (
-    <div>
 
-    </div>
-  );
+    const [flag, setFlag] = useState(true);
+
+    return (
+        <div>
+            {flag && <UserFc/>}
+            <button  onClick={()=> setFlag(false)}>hide</button>
+        </div>
+    );
 }
 
 export {App};
